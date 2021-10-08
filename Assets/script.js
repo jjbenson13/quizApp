c8f4d1af57bac904bf53008cc299b6cbf4ac494d
Jasmine Benson <jjortega2010@gmail.com>
2021-10-08 03:59:31 -0700

    test

---
+++ Assets/script.js
@@ -0,0 +1,91 @@
+const quizData = [
+  {
+    question: 'What language establishes the content of a webpage?',
+    a: 'HTML',
+    b: 'CSS',
+    c: 'JavaScript',
+    d: 'Ham',
+    correct: 'HTML',
+  }
+
+  {
+    question: 'DOM is an abbreviation for,
+    a: 'Data Object Mode',
+    b: 'Dumb Old Man',
+    c: 'Document Object Model',
+    d: 'Dutle Opo Mipsy',
+    correct: 'Document Object Model',
+  }
+
+  {
+    question: 'The condition in an if / else statement is inclosed within',
+    a: 'quotes',
+    b: 'curly brackets',
+    c: 'parentheses',
+    d: 'square brackets',
+    correct: "parentheses",
+  }
+
+  {
+    question: 'How would you console log Hello World?',
+    a: `console('Hello World')`,
+    b: `console.log('Hello World')`,
+    c: `function.write('Hello World')`,
+    d: `log.'Hello World'`,
+    correct: `console.log('Hello World')`,
+  }
+
+  {
+    question: 'Inside which HTML element do we put JavaScript?',
+    a: '< javascript >',
+    b: '< js >',
+    c: '< script >',
+    d: '< scripting >',
+    correct: '< script >',
+  }
+
+  {
+    question: 'How do you call a function named myFunction?',
+    a: 'call myFunction()',
+    b: 'myFunction()',
+    c: 'call function myFunction',
+    d: 'Call.myFunction()',
+    correct: 'myFunction()',
+  }
+
+  {
+    question: 'What is the correct place to insert JavaScript into HTML?',
+    a: '< head >',
+    b: '< body >',
+    c: 'Both',
+    d: 'Neither'
+    correct: 'Both',
+  }
+
+  {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    a: '< script src="xxx.js" >',
+    b: '< script name="xxx.js" >',
+    c: '< script href="xxx.js" >',
+    d: 'None of the above',
+    correct: '< script src="xxx.js"',
+  }
+
+  {
+    question: 'How do you write a conditional statement for executing some statements only if i is equal to 5?',
+    a: 'if i==5 then',
+    b: 'if (i == 5)',
+    c: 'if i=5 then',
+    d: 'if i=5',
+    correct: 'if (i == 5)',
+  }
+
+  {
+    question: 'Which is NOT a JavaScript data type?',
+    a: 'Number',
+    b: 'String',
+    c: 'Boolean',
+    d: 'Undeclared',
+    correct: 'Undeclared',
+  }
+]
